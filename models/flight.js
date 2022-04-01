c7957f9e5ce353d7e06b65d2b849540e657eb6e5
ivanmirandaortega <ivanmiranda@berkeley.edu>
2022-03-31 18:37:15 -0700

    updated flight schema properties

--- models/flight.js
+++ models/flight.js
@@ -2,10 +2,26 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const flightSchema = new Schema({
-    airline: [String],
-    airport: [String],
-    flightNo: Number,
-    departs: Date
+    airline: {
+        type: String,
+        enum: ['American', 'Delta', 'Southwest', 'United'],
+    },
+    airport: {
+        type: String,
+        enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN'
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Date,
+        default: function () {
+            return new Date(new Date().setFullYear(new Date().getFullYear() + 1))
+        }
+    }
 });
 
 module.exports = mongoose.model('Flight', flightSchema); 
