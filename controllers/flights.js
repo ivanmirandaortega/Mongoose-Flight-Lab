a2f8d485b501f1e1c0e1c8eb647be3f9b81cab02
ivanmirandaortega <ivanmiranda@berkeley.edu>
2022-03-31 21:18:18 -0700

    created config folder and functions

---
+++ controllers/flights.js
@@ -0,0 +1,28 @@
+const Flight = require('../models/flight');
+
+module.exports = {
+    index,
+    new: newFlight,
+    create
+};
+
+function newFlight(req, res) {
+    res.render('flights/new');
+};
+
+function create(req, res) {
+    const flight = new Flight(req.body);
+    flight.save(function (err) {
+        if (err) return res.render('flights/new');
+        console.log(flight);
+        res.redirect('/flights');
+    });
+};
+
+function index(req, res) {
+    Flight.find({}, function (err, flights) {
+        res.render('flights/index', {
+            flights
+        })
+    })
+}
